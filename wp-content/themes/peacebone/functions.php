05adcdf4c5bc06e46c28bf69ec6bfbf0b24e3673
Tommy Alter <tomealter@gmail.com>
2018-03-01 18:23:16 -0500

    added ACF export // uploaded video files

--- wp-content/themes/peacebone/functions.php
+++ wp-content/themes/peacebone/functions.php
@@ -63,6 +63,7 @@ function add_theme_scripts() {
 	
 	 wp_enqueue_style( 'styles', get_template_directory_uri() . '/css/styles.css', array(), '1.1', 'all');
 	 wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+	 wp_enqueue_script( 'script', get_template_directory_uri() . '/js/jquery.vide.js', array ( 'jquery' ), 1.1, true);
  }
  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
