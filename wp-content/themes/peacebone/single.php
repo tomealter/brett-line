9e786c7e7a7c2151228f6955f9421687a5bc164f
Tommy Alter <tomealter@gmail.com>
2018-04-04 00:24:33 -0400

    set up context for related posts // worked on theming single pages // themed the related section // added styles for fancy media

--- wp-content/themes/peacebone/single.php
+++ wp-content/themes/peacebone/single.php
@@ -13,6 +13,48 @@ $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
+// Query Related Film Posts
+if ( $post->post_type == 'film' ) {
+
+	$context['related_films'] = Timber::get_posts( 
+		array(
+			'post_type'   		=> 'film',
+			'posts_per_page' 	=> 2,
+			'post__not_in' 		=> array( $post->ID ),
+			'orderby'				=> 'rand'
+		)
+	);
+}
+
+//Query Related Photography Posts
+if ( $post->post_type == 'photo' ) {
+
+	// Context for related blog posts
+	$context['related_photos'] = Timber::get_posts( 
+		array(
+			'post_type'   		=> 'photo',
+			'posts_per_page' 	=> 2,
+			'post__not_in' 		=> array( $post->ID ),
+			'orderby'				=> 'rand'
+		)
+	);
+}
+
+//Query Related Graphic Posts
+if ( $post->post_type == 'graphic' ) {
+
+	// Context for related blog posts
+	$context['related_graphics'] = Timber::get_posts( 
+		array(
+			'post_type'   		=> 'graphic',
+			'posts_per_page' 	=> 2,
+			'post__not_in' 		=> array( $post->ID ),
+			'orderby'				=> 'rand'
+		)
+	);
+}
+
+
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
